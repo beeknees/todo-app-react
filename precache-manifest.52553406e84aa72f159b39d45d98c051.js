c0c32452720699b5e6d1dc939525ebbe6bd84210
Bettina Nicholson <bnichols@bnichols-osx-2.local>
2020-08-11 17:38:24 -0500

    Updates

---
+++ precache-manifest.52553406e84aa72f159b39d45d98c051.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b993f0c1ea702239800dfff43dba8311",
+    "url": "/todo-app-react/index.html"
+  },
+  {
+    "revision": "c283c32fcff550d780e7",
+    "url": "/todo-app-react/static/css/main.56df6992.chunk.css"
+  },
+  {
+    "revision": "468f9c4dfa7c5064d329",
+    "url": "/todo-app-react/static/js/2.354bc41a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/todo-app-react/static/js/2.354bc41a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c283c32fcff550d780e7",
+    "url": "/todo-app-react/static/js/main.680dc393.chunk.js"
+  },
+  {
+    "revision": "f436b522ebf7b87e6557",
+    "url": "/todo-app-react/static/js/runtime-main.6731cd9c.js"
+  }
+]);
